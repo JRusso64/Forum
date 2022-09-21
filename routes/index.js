32516260a11c24025a390113ba2b62bcceeb019b
John Russo <jrusso0604@gmail.com>
2022-09-20 20:34:59 -0400

    sorts by time of creation

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const Post = require('.././models/post')
 
 /* GET home page. */
 router.get('/', async function(req, res, next) {
-  const post = await Post.find()
+  const post = await Post.find().sort({ createdAt: 'desc'})
   res.render('index', { posts: post })
 });
 
